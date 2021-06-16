8519f79bb026a1e04a666e823443f57bf489ba45
E01T <etsouderos@gmail.com>
2021-06-16 18:38:05 +0300

    package & rollup.config update

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
-  import PhotoGallery from "./PhotoGallery.svelte";
+  import PhotoGallery from './PhotoGallery.svelte';
 </script>
 
 <main>
-  <PhotoGallery></PhotoGallery>
+  <PhotoGallery />
 </main>
 
 <style>
@@ -12,13 +12,6 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
   }
 
   @media (min-width: 640px) {
